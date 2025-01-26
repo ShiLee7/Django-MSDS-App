62c45a9d95ba3cdaaf1bf55b6d75f8b8d0c2cc1f
Leo <leonardo.jimenezmadrigal@gmail.com>
2025-01-25 20:05:07 -0600

    v4

--- SUN/settings.py
+++ SUN/settings.py
@@ -132,7 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+print(STATIC_URL)
+STATIC_ROOT = BASE_DIR / 'static'
+print(STATIC_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
